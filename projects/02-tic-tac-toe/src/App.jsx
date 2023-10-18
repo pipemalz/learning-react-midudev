5cbae813af4ed076a4c044891aee325f50614852
pipemalz <sinister012@gmail.com>
2023-10-18 17:13:39 -0500

    02-tic-tac-toe: Creando componentes y estados para los turnos

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,11 +1,33 @@
 import './App.css'
+import { useState } from 'react'
+import Square from './Square'
 
 function App() {
 
-  return (
-    <h1>Tic Tac Toe</h1>
-  )
+  const turns = {X:'X', O:'O'}
 
+  const board = new Array(9).fill(null)
+
+  const [turn, setTurn] = useState(turns.X)
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((position, index) => {
+            return (
+              <Square 
+                key={index} 
+                index={index}
+                turn={turn}
+              />
+            )
+          })
+        }
+      </section>
+    </main>
+  )
 }
 
 export default App
---
+++ projects/02-tic-tac-toe/src/Square.jsx
@@ -0,0 +1,11 @@
+function Square ({ children, index }) {
+
+
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
+export default Square
